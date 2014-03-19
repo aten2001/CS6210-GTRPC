6a3206e5977e192aee8dcc593420b57bd9e6f7c8
ddr2108 <deepdattaroy8888@gmail.com>
2014-03-19 19:08:43 -0400

    Added properly named files

---
+++ Work/Fifo/FifoCache_client.cpp
@@ -0,0 +1,46 @@
+#include "FifoCache.h"
+#include <curl/curl.h>
+#include <transport/TSocket.h>
+#include <transport/TBufferTransports.h>
+#include <protocol/TBinaryProtocol.h>
+#include <string.h>
+
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+using namespace Fifo;
+
+int main(int argc, char **argv) {
+	//Create the connection
+	boost::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
+	boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+	boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+
+	//Open connection to server
+	FifoCacheClient client(protocol);
+	transport->open();
+
+	//Call proxy
+	std::string url = "www.google.com";
+	std::string data;
+	client.getURL(data, url);
+
+	url = "www.yahoo.com";
+	client.getURL(data, url);
+
+	url = "www.google.com";
+	client.getURL(data, url);
+
+  
+	url = "www.bing.com";
+	client.getURL(data, url);
+
+	url = "www.cnn.com";
+	client.getURL(data, url);
+
+	//Close connection
+	transport->close();
+
+	return 0;
+}
---
+++ Work/Random/RandomCache_client.cpp
@@ -0,0 +1,44 @@
+#include "RandomCache.h"
+#include <transport/TSocket.h>
+#include <transport/TBufferTransports.h>
+#include <protocol/TBinaryProtocol.h>
+#include <string.h>
+
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+using namespace Random;
+
+int main(int argc, char **argv) {
+	//Create the connection
+	boost::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
+	boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+	boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+
+	//Open connection to server
+	RandomCacheClient client(protocol);
+	transport->open();
+
+	//Call proxy
+	std::string url = "www.google.com";
+	std::string data;
+	client.getURL(data, url);
+
+	url = "www.yahoo.com";
+	client.getURL(data, url);
+
+	url = "www.google.com";
+	client.getURL(data, url);
+
+	url = "www.yahoo.com";
+	client.getURL(data, url);
+  
+	url = "www.bing.com";
+	client.getURL(data, url);
+
+	//Close connection
+	transport->close();
+
+	return 0;
+}
