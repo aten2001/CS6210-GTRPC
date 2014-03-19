9d5d560173ac2aec85a396d0709e7812643892b3
Deep Datta Roy <deep@deep-MacbookVM.(none)>
2014-03-19 14:27:06 -0400

    Removed unnecessary files

--- Work/Part 1/compile.sh
+++
@@ -1,5 +0,0 @@
-thrift --gen cpp rpc.thrift
-cp Makefile gen-cpp/
-cp Something_client.cpp gen-cpp/
-cd gen-cpp
-mv Something_server.skeleton.cpp Something_server.cpp 
--- Work/Part 1/rpc.thrift
+++
@@ -1,5 +0,0 @@
-namespace cpp Test
-
-service Something {
-  string time(1:string url)
-}
